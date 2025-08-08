b9c6c63af26236b44f6e52cb07b5eff87de44117
Carlos <carlosaac232001@gmail.com>
2025-08-07 21:18:54 -0500

    feat(components): add header, form and footer web components
    - Header: mobile and desktop menus with animations\n- Form: structured fields and send button\n- Footer: navigation links and social icons\n- Include responsive styles for desktop where relevant

---
+++ components/header.js
@@ -0,0 +1,45 @@
+function main() {
+  const container = document.querySelector('.header__container');
+
+  container.innerHTML = `
+  <header class="header">
+      <a href="./index.html">
+        <img src="images/logo.svg" alt="Logo" class="header__logo" />
+      </a>
+
+      <img src="images/menu-open.svg" alt="Menu" class="header__menu-open" />
+
+      <div class="header__menu-mobile">
+        <div class="header__menu-toggle">
+          <img src="images/menu-close.svg" alt="Menu" class="header__menu-close" />
+        </div>
+        <ul class="header__menu-links">
+          <li class="header__menu-link">
+            <a href="./index.html">Home</a>
+          </li>
+          <li class="header__menu-link">
+            <a href="./services.html">Servicios</a>
+          </li>
+          <li class="header__menu-link">
+            <a href="./contact.html">Contacto</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="header__menu-desktop">
+        <ul class="header__desktop-links">
+          <li class="header__desktop-link">
+            <a href="./index.html">Home</a>
+          </li>
+          <li class="header__desktop-link">
+            <a href="./services.html">Servicios</a>
+          </li>
+          <li class="header__desktop-link">
+            <a href="./contact.html">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </header>`;
+}
+
+main();
