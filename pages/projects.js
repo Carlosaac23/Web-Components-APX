d527f6809de1fa8e334021833c82845c58ce3707
Carlos <carlosaac232001@gmail.com>
2025-08-09 14:33:06 -0500

    feat(projects): fetch and display projects from Contentful API

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,49 @@
-function main() {
+async function getProjects() {
+  const SPACE_ID = '6d07ippi0mdt';
+  const ENVIRONMENT_ID = 'master';
+  const CONTENT_TYPE_ID = 'work'; // 'work' is the ID of Proyectos
+  const ACCESS_TOKEN = 'BmlxfqdmBXnbx4-v7pdK5z41vqOyhZAfu9EVJmcAWsI';
+
+  const res = await fetch(
+    `https://preview.contentful.com/spaces/${SPACE_ID}/environments/${ENVIRONMENT_ID}/entries?access_token=${ACCESS_TOKEN}&content_type=${CONTENT_TYPE_ID}`
+  );
+
+  const data = await res.json();
+
+  return data.items.map(item => {
+    const imageId = item.fields.imagen.sys.id;
+    const asset = data.includes.Asset.find(asset => asset.sys.id === imageId);
+    const imageUrl = asset ? `https:${asset.fields.file.url}` : '';
+
+    return {
+      image: imageUrl,
+      title: item.fields.titulo || 'Sin título',
+      description: item.fields.descripcion || 'Sin descripción',
+      url: item.fields.url || '#',
+    };
+  });
+}
+
+function addProjectCard(project) {
+  const { image, title, description, url } = project;
+  console.log(image);
+  const projectsContainer = document.querySelector('.projects');
+  const projectCard = document.createElement('div');
+  projectCard.classList.add('projects__card');
+
+  projectCard.innerHTML = `
+    <img src=${image} alt="A project image" class="projects__card-img" />
+    <h2 class="projects__card-title">${title}</h2>
+    <p class="projects__card-description">${description}</p>
+    <a href=${url} target="_blank" class="projects__card-link">Ver proyecto
+    <img src="images/link.svg" alt="Link icon" class="projects__card-link-icon" />
+    </a>
+  `;
+
+  projectsContainer.appendChild(projectCard);
+}
+
+async function main() {
   const projectsContainer = document.querySelector('.projects__container');
 
   if (!projectsContainer) {
@@ -12,32 +57,16 @@ function main() {
       <img src="images/work.svg" alt="Suitcase icon" class="projects__img" />
     </main>
     
-   <div class="projects">
-      <div class="projects__card">
-        <img src="images/project1.png" alt="" class="projects__card-img" />
-        <h2 class="projects__card-title">Proyecto 1</h2>
-        <p class="projects__card-description">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum facere, asperiores obcaecati nemo, accusantium iusto vitae non autem accusamus quo.
-        </p>
-        <a href="#" class="projects__card-link">Ver más</a>
-      </div>
-      <div class="projects__card">
-        <img src="images/project1.png" alt="" class="projects__card-img" />
-        <h2 class="projects__card-title">Proyecto 2</h2>
-        <p class="projects__card-description">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum facere, asperiores obcaecati nemo, accusantium iusto vitae non autem accusamus quo.
-        </p>
-        <a href="#" class="projects__card-link">Ver más</a>
-      </div>
-      <div class="projects__card">
-        <img src="images/project1.png" alt="" class="projects__card-img" />
-        <h2 class="projects__card-title">Proyecto 3</h2>
-        <p class="projects__card-description">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum facere, asperiores obcaecati nemo, accusantium iusto vitae non autem accusamus quo.
-        </p>
-        <a href="#" class="projects__card-link">Ver más</a>
-      </div>
-    </div>`;
+   <div class="projects"></div>`;
+
+  try {
+    const projects = await getProjects();
+    for (const project of projects) {
+      addProjectCard(project);
+    }
+  } catch (error) {
+    console.error('Error cargando los proyectos:', error);
+  }
 }
 
 main();
